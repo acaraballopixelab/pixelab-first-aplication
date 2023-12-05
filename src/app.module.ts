a5ab45fd9c6f84eae00fdf3cfc6f4c80352d4178
Adrian Caraballo <carabalapp@gmail.com>
2023-12-05 19:00:05 -0400

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // Le coloco el + adelante para convertirlo en numero ya que sino nos arroja error por tomarlo como string
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ]
+})
+export class AppModule {}
